2eccf8f1aa47596e8599fd793ff575ad9ed0fa8f
Michael Adams <mtadams42@gmail.com>
2025-03-08 11:04:59 -0800

    Same thing, but I tried a more precise overlap method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import geopandas
 import csv
 import statistics
+from shapely.geometry import Polygon
 
 
 def load_shapes():
@@ -18,7 +19,7 @@ def load_shapes():
     return gdf
 
 
-def get_mapping():
+def get_unweighted_mapping():
     # key is zip code, value is array of air quality measurements
     mapping = {}
     shapes = load_shapes()
@@ -27,7 +28,10 @@ def get_mapping():
     # min_x, max_x, min_y, max_y, air quality
     with open('./data/boxes.csv', newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        count = 0
         for point in csvreader:
+            count += 1
+            if count % 100 == 0: print(f"Finished {count} rows")
             min_x = float(point[0])
             max_x = float(point[1])
             min_y = float(point[2])
@@ -44,8 +48,58 @@ def get_mapping():
                 mapping.setdefault(zip, []).append(air_quality)
     return mapping
 
+# NOTE: I haven't actually verified if this is correct.
+def get_weighted_mapping():
+    # key is zip code, value is array of tuples of (value, weight)
+    mapping = {}
+    shapes = load_shapes()
+    # This assumes your dataset is just a csv and each row is a bounding box
+    # and then the air quality, i.e.
+    # min_x, max_x, min_y, max_y, air quality
+    with open('./data/boxes.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        count = 0
+        for point in csvreader:
+            count += 1
+            if count % 100 == 0: print(f"Finished {count} rows")
+            min_x = float(point[0])
+            max_x = float(point[1])
+            min_y = float(point[2])
+            max_y = float(point[3])
 
-for key, values in get_mapping().items():
-    # Basic, unweighted mean
-    avg = statistics.mean(values)
-    print(key, avg)
+            # I'm not sure what this number actually is
+            air_quality = float(point[4])
+
+            zips_gdf = shapes.cx[min_x:max_x, min_y:max_y]
+            aq_gdf = geopandas.GeoDataFrame(data={
+              'geometry':[
+                  Polygon([(min_x,min_y),(min_x,max_y),(max_x,max_y),(max_x,min_y)]),
+              ]}, geometry='geometry')
+            for index, row in zips_gdf.iterrows():
+                zip_gdf = geopandas.GeoDataFrame([row])
+                gdf_joined = geopandas.overlay(zip_gdf, aq_gdf, how='intersection')
+                intersected_area = gdf_joined.area[0]
+                zipcode_area = zip_gdf.area[0]
+                weight = intersected_area / zipcode_area
+                zip = zip_gdf["GEOID20"][0]
+                mapping.setdefault(zip, []).append((air_quality, weight))
+    return mapping
+
+# Faster, less precise
+def fast_and_imprecise():
+    for key, values in get_unweighted_mapping().items():
+        avg = statistics.mean(values)
+        print(f"{key},{avg}")
+
+# Slower, more precise
+def slow_and_precise():
+    for key, values in get_weighted_mapping().items():
+        weighted_sum = sum(value * weight for value, weight in values)
+        total_weight = sum(weight for _, weight in values)
+
+        # Weighted mean
+        avg = weighted_sum / total_weight
+        print(f"{key},{avg}")
+
+fast_and_imprecise()
+# slow_and_precise()
