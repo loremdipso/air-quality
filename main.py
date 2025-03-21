bdb39ef0a6067a7a5796a0fdf4a7c65c8ae8e6da
Michael Adams <mtadams42@gmail.com>
2025-03-21 10:18:37 -0700

    Updated for new file format

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ import statistics
 from shapely.geometry import Polygon
 
 
-def load_shapes():
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "--convert":
+            convert()
+            return
+        elif sys.argv[1] == "--slow":
+            num_rows = slow_and_precise()
+            print(f"Done writing {num_rows} rows to output.csv")
+            return
+        elif sys.argv[1] == "--fast":
+            num_rows = fast_and_imprecise()
+            print(f"Done writing {num_rows} rows to output.csv")
+            return
+    print("Usage: ./main.py [--slow|--fast]")
+
+
+def load_census_blocks():
+    print("Loading census blocks...")
     bbox = (
-        # Rough square around washington
+        # Rough square around washington to limit our search space
+        # TODO: button this up
         -130,
         50,
         -115, 
@@ -16,62 +34,102 @@ def load_shapes():
 
     # Performance: you might want to build a spatial index:
     # https://geopandas.org/en/stable/docs/reference/sindex.html
-    gdf = geopandas.read_file("./data/tl_2020_us_zcta520/tl_2020_us_zcta520.shp", bbox=bbox)
+
+    # I downloaded the GeoPackage file from https://hub.arcgis.com/datasets/d795eaa6ee7a40bdb2efeb2d001bf823_0/about
+    # It says it was published in 2021 and updated in 2025, so I'm hopeful that this is pretty up to date.
+    # gdf = geopandas.read_file("./data/census_blocks/census_blocks.gpkg", bbox=bbox)
+    # gdf = geopandas.read_file("./data/census_blocks/census_blocks.gpkg")
+    gdf = geopandas.read_file("./data/census_blocks/census_blocks.gpkg")
+
+    # Change the coordinate reference system to something more standard.
+    # This one is long/lat with 2M accuracy, which should be more than fine.
+    print("Converting to a reasonable format")
+    gdf.to_crs(epsg=4326, inplace=True)
     return gdf
+
+
+# Faster, less precise
+def fast_and_imprecise():
+    num_rows = 0
+    with open("output.csv", "w") as output_file:
+        output_file.write("Block ID,Average Air Quality\n")
+        for key, values in get_unweighted_mapping().items():
+            avg = statistics.mean(values)
+            output_file.write(f"{key},{avg}\n")
+            num_rows += 1
+    return num_rows
+
+
+# Slower, more precise
+def slow_and_precise():
+    num_rows = 0
+    with open("output.csv", "w") as output_file:
+        output_file.write("Block ID,Average Air Quality\n")
+        for key, values in get_weighted_mapping().items():
+            weighted_sum = sum(value * weight for value, weight in values)
+            total_weight = sum(weight for _, weight in values)
+
+            # Weighted mean
+            avg = weighted_sum / total_weight
+            output_file.write(f"{key},{avg}\n")
+            num_rows += 1
+    return num_rows
 
 
 def get_unweighted_mapping():
     # key is zip code, value is array of air quality measurements
     mapping = {}
-    shapes = load_shapes()
+    census_blocks = load_census_blocks()
     # This assumes your dataset is just a csv and each row is a bounding box
     # and then the air quality, i.e.
     # min_x, max_x, min_y, max_y, air quality
-    with open('./data/boxes.csv', newline='') as csvfile:
+    print("Running through out air quality boxes...")
+    with open('./data/air_quality_boxes.csv', newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         count = 0
-        for point in csvreader:
+        for air_quality_rect in csvreader:
             count += 1
             if count % 100 == 0: print(f"Finished {count} rows")
-            min_x = float(point[0])
-            max_x = float(point[1])
-            min_y = float(point[2])
-            max_y = float(point[3])
+            min_x = float(air_quality_rect[0])
+            max_x = float(air_quality_rect[1])
+            min_y = float(air_quality_rect[2])
+            max_y = float(air_quality_rect[3])
 
             # I'm not sure what this number actually is
-            air_quality = float(point[4])
-
-            result = shapes.cx[min_x:max_x, min_y:max_y]
+            air_quality = float(air_quality_rect[4])
 
             # This finds all overlapping ZCTAs. If you want more precision you
             # could check a bunch of points and see how many overlap.
-            for zip in result["GEOID20"]:
-                mapping.setdefault(zip, []).append(air_quality)
+            result = census_blocks.cx[min_x:max_x, min_y:max_y]
+            for block_id in result["BLOCK"]:
+                mapping.setdefault(block_id, []).append(air_quality)
     return mapping
+
 
 # NOTE: I haven't actually verified if this is correct.
 def get_weighted_mapping():
     # key is zip code, value is array of tuples of (value, weight)
     mapping = {}
-    shapes = load_shapes()
+    census_blocks = load_census_blocks()
     # This assumes your dataset is just a csv and each row is a bounding box
     # and then the air quality, i.e.
     # min_x, max_x, min_y, max_y, air quality
-    with open('./data/boxes.csv', newline='') as csvfile:
+    print("Running through out air quality boxes...")
+    with open('./data/air_quality_boxes.csv', newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         count = 0
-        for point in csvreader:
+        for air_quality_rect in csvreader:
             count += 1
             if count % 100 == 0: print(f"Finished {count} rows")
-            min_x = float(point[0])
-            max_x = float(point[1])
-            min_y = float(point[2])
-            max_y = float(point[3])
+            min_x = float(air_quality_rect[0])
+            max_x = float(air_quality_rect[1])
+            min_y = float(air_quality_rect[2])
+            max_y = float(air_quality_rect[3])
 
             # I'm not sure what this number actually is
-            air_quality = float(point[4])
+            air_quality = float(air_quality_rect[4])
 
-            zips_gdf = shapes.cx[min_x:max_x, min_y:max_y]
+            zips_gdf = census_blocks.cx[min_x:max_x, min_y:max_y]
             aq_gdf = geopandas.GeoDataFrame(data={
               'geometry':[
                   Polygon([(min_x,min_y),(min_x,max_y),(max_x,max_y),(max_x,min_y)]),
@@ -91,30 +149,5 @@ def get_weighted_mapping():
                 mapping.setdefault(zip, []).append((air_quality, weight))
     return mapping
 
-# Faster, less precise
-def fast_and_imprecise():
-    for key, values in get_unweighted_mapping().items():
-        avg = statistics.mean(values)
-        print(f"{key},{avg}")
-
-# Slower, more precise
-def slow_and_precise():
-    for key, values in get_weighted_mapping().items():
-        weighted_sum = sum(value * weight for value, weight in values)
-        total_weight = sum(weight for _, weight in values)
-
-        # Weighted mean
-        avg = weighted_sum / total_weight
-        print(f"{key},{avg}")
-
-def main():
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "--slow":
-            slow_and_precise()
-            return
-        elif sys.argv[1] == "--fast":
-            fast_and_imprecise()
-            return
-    print("Usage: ./main.py [--slow|--fast]")
 
 main()
